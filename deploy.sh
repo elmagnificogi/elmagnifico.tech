2532a66a64af609c3ca36240577ee29cae8842c5
elmagnifico <elmagnificogg@gmail.com>
2021-05-13 11:05:52 +0800

    update-post

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+result=$(cd /root/elmagnificogi.github.io && git pull origin master | grep "Already up-to-date" )
+if [[ "$result" != "" ]]
+then
+  exit 0
+else
+  echo "`date '+%Y%m%d %H:%M'`: post update,start build"
+  result=$(jekyll build --source /root/elmagnificogi.github.io --destination /usr/share/nginx/html)
+  echo $result
+  echo "`date '+%Y%m%d %H:%M'`: build over"
+fi
